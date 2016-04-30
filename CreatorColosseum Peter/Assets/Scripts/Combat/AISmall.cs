5d97a5046dd36a211bcfffef0c6e644c54c839d1
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-04-29 22:40:03 -0500

    Enemy Nav ReAdded

--- CreatorColosseum Peter/Assets/Scripts/Combat/AISmall.cs
+++ CreatorColosseum Peter/Assets/Scripts/Combat/AISmall.cs
@@ -29,6 +29,10 @@ public class AISmall : MonoBehaviour {
 	private float permentTimer;
 
 	public Transform targetPlayer;
+	public Transform targetObject;
+	public Transform targetRayCast;
+
+	private Vector3 vectorDestination;
 
 	public GameObject _player;
 	private GameObject _enemy;
@@ -155,8 +159,174 @@ public class AISmall : MonoBehaviour {
 		else if (playerC.gameObject.tag == "Enemy")
 		{
 			enemyTouch = true;
+			_enemy = playerC.gameObject;
+			
+			if (_enemy.GetComponent<AISmall>().movingUp)
+			{
+				enemyNav = true;
+				// print("he's going up!");
+				
+				hit = RayLeft(targetRayCast); //check left
+				
+				if (hit.collider == null)
+				{
+					//print("move to the left!");
+					targetObject.position = vectorDestination;
+				}
+				else
+				{
+					hit = RayRight(targetRayCast); //check right
+					
+					if (hit.collider == null)
+					{
+						//print("move to the right!");
+						targetObject.position = vectorDestination;
+					}
+					else
+					{
+						hit = RayUp(targetRayCast); //check up
+						if (hit.collider == null)
+						{
+							// print("move to the up!");
+							targetObject.position = vectorDestination;
+						}
+						else
+						{
+							hit = RayDown(targetRayCast); //check down
+							if (hit.collider == null)
+							{
+								//print("move to the down!");
+								targetObject.position = vectorDestination;
+							}
+							else
+							{
+								//print("HELP I'M TRAPPED!");
+								enemyNav = false;
+							}
+						}
+					}
+				}
+				
+			}
+			
+			else if (_enemy.GetComponent<AISmall>().movingRight)
+			{
+				enemyNav = true;
+				//print("he's going right!");
+				
+				hit = RayUp(targetRayCast); //check up
+				if (hit.collider == null)
+				{
+					//print("move to the up!");
+					targetObject.position = vectorDestination;
+				}
+				else
+				{
+					hit = RayDown(targetRayCast); //check down
+					
+					if (hit.collider == null)
+					{
+						//print("move to the down!");
+						targetObject.position = vectorDestination;
+					}
+					else
+					{
+						
+						hit = RayLeft(targetRayCast); //check left
+						
+						if (hit.collider == null)
+						{
+							//print("move to the left!");
+							targetObject.position = vectorDestination;
+						}
+						else
+						{
+							hit = RayRight(targetRayCast); // check right
+							
+							if (hit.collider == null)
+							{
+								//print("move to the right!");
+								targetObject.position = vectorDestination;
+							}
+							else
+							{
+								// print("HELP I'M TRAPPED~");
+								enemyNav = false;
+							}
+						}
+					}
+				}
+			}
+			else //playerTouch = true
+			{
+				// print("This guy's attacking....");
+				
+				_collider = playerC.collider;
+				
+				if (_collider.bounds.max.x <= _ownCollider.bounds.min.x || _collider.bounds.min.x > _ownCollider.bounds.max.x) //on the left or right side
+				{
+					//numHits++;
+					vectorDestination = new Vector3(_ownCollider.bounds.center.x, (_ownCollider.bounds.center.y + 6), 0);
+					playerC.gameObject.GetComponent<AISmall>().targetObject.position = vectorDestination;
+					// print("We're going around!");
+					enemyNav = true;
+				}
+				else
+				{
+					//numHits++;
+					vectorDestination = new Vector3((_ownCollider.bounds.center.x + 6), _ownCollider.bounds.center.y, 0);
+					targetObject.position = vectorDestination;
+					//print("We're going up!");
+					enemyNav = true;
+				}
+			}
+			}
 		}
-	}
+		RaycastHit2D RayLeft(Transform targetRayCast)
+		{
+			targetRayCast.transform.localPosition = new Vector3(-1, 0, 0);
+			hit = Physics2D.Raycast(targetRayCast.transform.position, -Vector2.right, 2); //check if left is open
+			//hit = Physics2D.BoxCast (targetRayCast.transform.position, new Vector2 (_ownCollider.bounds.min.x - 3, _ownCollider.bounds.min.y), 0, -Vector2.right);
+			Debug.DrawRay(targetRayCast.transform.position, -Vector2.right, Color.green, 2);
+			
+			vectorDestination = new Vector3((_ownCollider.bounds.min.x - 3), _ownCollider.bounds.center.y, 0);
+			//numHits++;
+			return hit;
+		}
+		
+		RaycastHit2D RayRight(Transform targetRayCast)
+		{
+			targetRayCast.transform.localPosition = new Vector3(1, 0, 0);
+			hit = Physics2D.Raycast(targetRayCast.transform.position, Vector2.right, 2); //check if right is open
+			Debug.DrawRay(targetRayCast.transform.position, Vector2.right, Color.red, 2);
+			
+			vectorDestination = new Vector3((_ownCollider.bounds.min.x + 3), _ownCollider.bounds.center.y, 0);
+			
+			//numHits++;
+			return hit;
+		}
+		
+		RaycastHit2D RayUp(Transform targetRayCast)
+		{
+			targetRayCast.transform.localPosition = new Vector3(0, 2, 0);
+			hit = Physics2D.Raycast(targetRayCast.transform.position, Vector2.up, 2); //check if up is open
+			Debug.DrawRay(targetRayCast.transform.position, Vector2.up, Color.black, 2);
+			
+			vectorDestination = new Vector3(_ownCollider.bounds.min.x, (_ownCollider.bounds.center.y + 4), 0);
+			//numHits++;
+			return hit;
+		}
+		
+		RaycastHit2D RayDown(Transform targetRayCast)
+		{
+			targetRayCast.transform.localPosition = new Vector3(0, -2, 0);
+			hit = Physics2D.Raycast(targetRayCast.transform.position, -Vector2.up, 2); //check if down is open
+			Debug.DrawRay(targetRayCast.transform.position, -Vector2.up, Color.yellow, 2);
+			
+			vectorDestination = new Vector3(_ownCollider.bounds.min.x, (_ownCollider.bounds.center.y - 4), 0);
+			//numHits++;
+			return hit;
+		}
 
 	void OnCollisionStay2D(Collision2D playerC)
 	{
