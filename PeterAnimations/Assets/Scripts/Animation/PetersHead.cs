de684fc5a37c361214f046b3323a39cab56e0206
Koesterc <Koesterc@gmatc.matc.edu>
2016-05-03 15:42:22 -0500

    Peter's Animations
    Combat script updated, player movement script updated, added animator,
    animation clips, and added Jeff's sprites. everything can be found in
    the animations folder.

---
+++ PeterAnimations/Assets/Scripts/Animation/PetersHead.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetersHead : MonoBehaviour {
+	public float smooth = 5.0f;
+	public float tiltAngle = 30.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float tiltAroundZ = Input.GetAxis ("Horizontal") * tiltAngle;
+		Quaternion target = Quaternion.Euler (0, 0, tiltAroundZ);
+		transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * smooth);
+	}
+}
