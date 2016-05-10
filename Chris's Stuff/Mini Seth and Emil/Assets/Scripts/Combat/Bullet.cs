de2147c68d5b03304f1d0b166fc3a48c0d18940c
Koesterc <Koesterc@gmatc.matc.edu>
2016-05-10 14:44:10 -0500

    Mini seth and mini emil's animations
    no need for the enemy scripts - you already have it.
    
    animators, animation clips, sprites etc.

---
+++ Chris's Stuff/Mini Seth and Emil/Assets/Scripts/Combat/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject _player;
+    public GameObject levelSelect;
+
+    public int movingSpeed;
+    public int bulletDamage;
+    public int xMin;
+    private int armor;
+
+    public Vector2 destination;
+
+    // Use this for initialization
+    void Start()
+    {
+        destination = new Vector2(xMin, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            MovePhase();
+        }
+
+        if (levelSelect.GetComponent<Transitions>().levelSelect > 1)
+        {
+            destination = new Vector2(500, 0);
+        }
+
+    }
+
+    void MovePhase()
+    {
+        if (destination.x < transform.position.x)
+        {
+            transform.position += transform.right * -movingSpeed * Time.deltaTime;
+        }
+        else
+            this.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D playerC)
+    {
+        if (playerC.gameObject.tag == "Player")
+        {
+            armor = _player.GetComponent<CombatScript>().armor;
+            _player.GetComponent<PlayerReceivesDamage>().InitiateCBT(bulletDamage.ToString()).GetComponent<Animator>().SetTrigger("Hit"); //changed playerReceivesDamge
+            _player.GetComponent<CombatScript>().health -= (bulletDamage - armor);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Chris's Stuff/Mini Seth and Emil/Assets/Scripts/Combat/LaserBig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBig : MonoBehaviour {
+	
+	public float	bigLaserDamage;
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag.Equals("Player"))
+		{
+			other.GetComponent<PlayerReceivesDamage> ().InitiateCBT (bigLaserDamage.ToString ()).GetComponent<Animator> ().SetTrigger ("Hit"); //changed playerReceivesDamge
+			other.GetComponent<CombatScript> ().health -= (bigLaserDamage - other.GetComponent<CombatScript> ().armor);
+		}
+	}
+}
